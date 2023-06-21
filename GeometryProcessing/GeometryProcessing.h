c48d0ef1c9f16092a819c80a1589c8b675d77158
RezaHemadi <r.hemadi@gmail.com>
2023-06-21 11:55:49 +0330

    Initial Commit

---
+++ GeometryProcessing/GeometryProcessing.h
@@ -0,0 +1,18 @@
+//
+//  GeometryProcessing.h
+//  GeometryProcessing
+//
+//  Created by Reza on 6/21/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GeometryProcessing.
+FOUNDATION_EXPORT double GeometryProcessingVersionNumber;
+
+//! Project version string for GeometryProcessing.
+FOUNDATION_EXPORT const unsigned char GeometryProcessingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeometryProcessing/PublicHeader.h>
+
+
